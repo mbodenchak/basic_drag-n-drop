0ba4274d1f283836bf1f7d13e9c5a7dc983852e5
mbodenchak <mbodenchak@outlook.com>
2022-04-26 09:50:14 -0700

    randomized array on page load

--- script.js
+++ script.js
@@ -22,18 +22,22 @@ createList();
 
 //Insert List Items into Dom
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("data-index", index);
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
 
-    listItem.innerHTML = `<span class="number">${index + 1}</span>
+      listItem.innerHTML = `<span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
     <p class="person-name">${person}</p>
     <i class="fas fa-grip-lines"></i>
     </div>`;
 
-    listItems.push(listItem);
+      listItems.push(listItem);
 
-    draggable_list.appendChild(listItem);
-  });
+      draggable_list.appendChild(listItem);
+    });
 }
